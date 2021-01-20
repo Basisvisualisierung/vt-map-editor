e848c7ff8ea217904b425d695ff51e0ed9a14011
David Mielke <david.mielke@lgln.niedersachsen.de>
2021-01-20 07:52:37 +0100

    test: added e2e test cases

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,110 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser, by, element, logging} from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
+    const sleep = 0;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to vt-map-editor!');
-  });
+    it('should display Title', () => {
+        page.navigateTo();
+        expect(page.getTitleText()).toEqual('Basisvisualisierung');
+        browser.sleep(sleep);
+    });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+    it('should be possible to change color of guiLayer', () => {
+        page.navigateTo();
+        page.clickToolButton(1);
+        page.clickDetailButton();
+        page.openGuiLayerGroup(0);
+        page.changeColor('100', '150', '111', '0.5');
+        expect(page.getColor()).toEqual('rgba(100, 150, 111, 0.5)');
+        browser.sleep(sleep);
+    });
+
+    it('should be possible to change color of layers', () => {
+        page.navigateTo('/edit/layer');
+        page.openLayerElement(0);
+        page.changeLayerColor('#503616');
+        expect(page.getColor()).toEqual('rgba(80, 54, 22, 1)' );
+        browser.sleep(sleep);
+    });
+
+    it('should be possible to show tooltip on map', () => {
+        page.navigateTo();
+        page.clickMap(200, 750);
+        expect(page.getTooltipStatus()).toBe(true);
+        browser.sleep(sleep * 3);
+    });
+
+    describe('tests related to functions declaration', () => {
+
+        it('should be possible to remove control elements from the map', () => {
+            page.navigateTo('/functions');
+            page.clickSlider(0);
+            expect(page.getZoomControlStatus()).toBe(false);
+            browser.sleep(sleep);
+        });
+
+        it('should be possible to remove function to create tooltip from the map', () => {
+            page.navigateTo('/functions');
+            page.clickSlider(1);
+            page.clickMap(200, 750);
+            expect(page.getTooltipStatus()).toBe(false);
+            browser.sleep(sleep * 3);
+        });
+
+        it('should be possible to remove search from the map', () => {
+            page.navigateTo('/functions');
+            page.clickSlider(2);
+            expect(page.getSearchStatus()).toBe(false);
+            browser.sleep(sleep);
+        });
+    });
+
+
+    describe('tests related to navigation', () => {
+
+        it('should navigate to /basemap if basemap is selected from toolbar', () => {
+            page.navigateTo();
+            page.clickToolButton(0);
+            expect(browser.getCurrentUrl()).toEqual(`${browser.baseUrl}/basemap`);
+        });
+
+        it('should navigate to /edit/group-layer if layers is selected from toolbar', () => {
+            page.navigateTo();
+            page.clickToolButton(1);
+            expect(browser.getCurrentUrl()).toEqual(`${browser.baseUrl}/edit/group-layer`);
+        });
+
+        it('should navigate to /edit/gui-layer if details is selected under group-layer', () => {
+            page.navigateTo();
+            page.clickToolButton(1);
+            page.clickDetailButton();
+            expect(browser.getCurrentUrl()).toEqual(`${browser.baseUrl}/edit/gui-layer`);
+        });
+
+        it('should navigate to /functions if settings is selected from toolbar', () => {
+            page.navigateTo();
+            page.clickToolButton(2);
+            expect(browser.getCurrentUrl()).toEqual(`${browser.baseUrl}/functions`);
+        });
+
+        it('should navigate to /share if share is selected from toolbar', () => {
+            page.navigateTo();
+            page.clickToolButton(3);
+            expect(browser.getCurrentUrl()).toEqual(`${browser.baseUrl}/share`);
+        });
+    });
+
+    afterEach(async () => {
+// Assert that there are no errors emitted from the browser
+        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        expect(logs).not.toContain(jasmine.objectContaining({
+            level: logging.Level.SEVERE,
+        } as logging.Entry));
+    });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,101 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
-  }
+    toolButtons = element.all(by.css('.tool-button'));
+    detailButtons = element.all(by.css('.btn-detail'));
+    colorButtons = element.all(by.css('.color-btn'));
+    canvas = element.all(by.css('.mapboxgl-canvas'));
+    rgbaTextField = element.all(by.css('.rgba-text'));
+    rgbaInput = this.rgbaTextField.all(by.css('input'));
+    okButton = element.all(by.css('.cp-ok-button-class'));
+    hexTextField = element.all(by.css('.hex-text'));
+    hexInput = this.hexTextField.all(by.css('input'));
+    layerDropdown = element.all(by.css('.mat-expansion-panel'));
+    headlineTitle = element(by.css('.header-title'));
+    tooltip = element.all(by.css('.mapboxgl-popup-content'));
+    zoomButton = element.all(by.css('.mapboxgl-ctrl-zoom-in'));
+    searchButton = element.all(by.css('.search-control'));
+
+    navigateTo(path: string = '') {
+        return browser.get(`${browser.baseUrl}${path}`) as Promise<any>;
+    }
+
+    getTitleText() {
+        return this.headlineTitle.getText();
+    }
+
+    getTooltipStatus() {
+        return this.tooltip.isPresent();
+    }
+
+    getZoomControlStatus(){
+        return this.zoomButton.isPresent();
+    }
+
+    getSearchStatus() {
+        return this.searchButton.isPresent();
+    }
+
+    clickToolButton(index: number = 1) {
+        this.toolButtons.get(index).click();
+    }
+
+    clickDetailButton() {
+
+        this.detailButtons.get(0).click();
+    }
+
+    clickSlider(index: number) {
+        element.all(by.css('.mat-slide-toggle-bar')).get(index).click();
+    }
+
+    openGuiLayerGroup(index: number = 0) {
+        const guiLayerGroup = element(by.id(`mat-expansion-panel-header-${index}`));
+        guiLayerGroup.click();
+        browser.sleep(500);
+    }
+
+    clickColorButton(index = 0 ) {
+        this.colorButtons.get(index).click();
+    }
+
+    clickMap(toRight: number, toBottom: number) {
+        browser.sleep(500);
+        browser.actions()
+            .mouseMove(this.canvas.get(0).getWebElement(), {
+                x: toRight,
+                y: toBottom,
+            })
+            .perform().then(() => browser.actions()
+                .click()
+                .perform());
+    }
+
+    changeColor(r: string, g: string, b: string, a: string) {
+        this.clickColorButton();
+        this.rgbaInput.clear();
+        this.rgbaInput.get(0).sendKeys(r);
+        this.rgbaInput.get(1).sendKeys(g);
+        this.rgbaInput.get(2).sendKeys(b);
+        this.rgbaInput.get(3).sendKeys(a);
+        this.okButton.get(0).click();
+    }
+
+    changeLayerColor(hexColor: string) {
+        this.clickColorButton();
+        this.hexInput.clear();
+        this.hexInput.get(0).sendKeys(hexColor);
+        this.okButton.get(0).click();
+    }
+
+    getColor(index: number = 0) {
+        browser.sleep(500);
+        return this.colorButtons.get(index).getCssValue('background-color');
+    }
+
+
+    openLayerElement(index: number) {
+        this.layerDropdown.get(index).click();
+    }
 }
